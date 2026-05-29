49a96ff2c9e5a4a9bffeb2d825405fdfbb97a2c0
Shiwam Dutta <shiwamdutta3277@gmail.com>
2026-05-29 15:28:31 +0530

    test: refine multilier 8-bit test cases

--- tb/multiplier_tb.v
+++ tb/multiplier_tb.v
@@ -1,30 +1,31 @@
 `timescale 1ns/1ps
 
 module multiplier_8bit_tb;
+    localparam WIDTH = 16;
 
-    reg  signed [7:0] a, b;
-    wire signed [15:0] product;
+    reg  signed [WIDTH-1:0] a, b;
+    wire signed [2*WIDTH-1:0] product;
     wire zero;
 
-    multiplier #(8) uut(a, b, product, zero);
+    multiplier #(WIDTH) uut(a, b, product, zero);
 
     initial begin
         $dumpfile("waves/multiplier.vcd");
         $dumpvars(0, uut);
 
         $monitor("%d * %d = %d", a, b, product);
-        a=   5; b=   3; #10;
-        a=-  5; b=   3; #10; // a = negative
-        a=   7; b=-  4; #10; // b = negative
-        a=-  6; b=-  6; #10; // both negatives
-        a=-128; b= 127; #10; // minimum * maximum
-        a=-128; b=-128; #10; // both minimum
-        a= 127; b= 127; #10; // both maximum
-        a=   0; b=  25; #10; // a = 0
-        a=- 45; b=   0; #10; // b = 0
-        a=   0; b=   0; #10; // both zero
-        a=   1; b=-128; #10; // multiplicative identity
-        a=-  1; b= 127; #10; // sign change
+        a = 5;    b = 3;     #10;
+        a = -5;   b = 3;     #10; // a = negative
+        a = 7;    b = -4;    #10; // b = negative
+        a = -6;   b = -6;    #10; // both negatives
+        a = -128; b = 127;   #10; // minimum * maximum
+        a = -128; b = -128;  #10; // both minimum
+        a = 127;  b = 127;   #10; // both maximum
+        a = 0;    b = 25;    #10; // a = 0
+        a = -45;  b = 0;     #10; // b = 0
+        a = 0;    b = 0;     #10; // both zero
+        a = 1;    b = -128;  #10; // identity element
+        a = -1;   b = 127;   #10; // sign change
 
         $finish;
     end
