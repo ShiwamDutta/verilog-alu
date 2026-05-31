2e0fc3feaba2118a6a223acaa42a172b14aa16e8
Shiwam Dutta <shiwamdutta3277@gmail.com>
2026-05-31 14:53:13 +0530

    test: refine alu 8-bit test cases

--- tb/alu_tb.v
+++ tb/alu_tb.v
@@ -1,15 +1,16 @@
 `timescale 1ns/1ps
 
 module alu_tb;
+    localparam WIDTH = 8;
 
-    reg signed [7:0] a, b;
+    reg signed [WIDTH-1:0] a, b;
     reg [3:0] op;
 
-    wire signed [15:0] result;
+    wire signed [2*WIDTH-1:0] result;
     wire carry, overflow, zero, div_by_zero;
 
     // DUT
-    alu #(8) uut (
+    alu #(WIDTH) uut (
         .a(a),
         .b(b),
         .op(op),
@@ -24,47 +25,25 @@ module alu_tb;
         $dumpfile("waves/alu.vcd");
         $dumpvars(0, uut);
 
-        $monitor("%h  %d    %d    %d | %d  %d  %d  %d",
-            op, a, b, result, overflow, carry, zero, div_by_zero);
+        $monitor(
+            "%h  %d    %d    %d | %d  %d  %d  %d",
+            op, a, b, result, overflow, carry, zero, div_by_zero
+        );
+        op = 4'h0; a = 127; b =  1;  #10; // ADD -> overflow
+        op = 4'h1; a =  25; b = 25;  #10; // SUB -> zero
+        op = 4'h2; a =  -5; b =  3;  #10; // MUL
+        op = 4'h3; a = 100; b =  0;  #10; // DIV -> div by zero
 
-        // ADD
-        op=4'h0; a=10; b=5;    #10;
-        op=4'h0; a=200; b=100; #10;  // overflow
-
-        // SUB
-        op=4'h1; a=20; b=5;  #10;
-        op=4'h1; a=5; b=20;  #10;    // negative result
-
-        // MUL
-        op=4'h2; a=7; b=6;   #10;
-        op=4'h2; a=15; b=15; #10;
-
-        // DIV
-        op=4'h3; a=20; b=4;  #10;
-        op=4'h3; a=7;  b=2;  #10;
-        op=4'h3; a=10; b=0;  #10;    // divide by zero
-
-        $monitor("%h    %h      %h      %h | %d  %d  %d  %d",
-            op, a, b, result, overflow, carry, zero, div_by_zero);
-        
-        // AND
-        op=4'h4; a=8'hAA; b=8'h55; #10;
-
-        // OR
-        op=4'h5; a=8'hF0; b=8'h0F; #10;
-
-        // XOR
-        op=4'h6; a=8'hFF; b=8'h0F; #10;
-
-        // NOT
-        op=4'h7; a=8'h00; b=8'hFF; #10;
-        op=4'h7; a=8'hFF; b=8'h00; #10;
-
-        // PASS A
-        op=4'hE; a=123; b=45; #10;
-
-        // PASS B
-        op=4'hF; a=123; b=45; #10;
+        $monitor(
+            "%h    %h      %h      %h | %d  %d  %d  %d",
+            op, a, b, result, overflow, carry, zero, div_by_zero
+        );
+        op = 4'h4; a = 8'hAA; b = 8'h55;  #10; // AND -> alternating pattern
+        op = 4'h5; a = 8'hF0; b = 8'h0F;  #10; // OR -> all set
+        op = 4'h6; a = 8'h26; b = 8'h26;  #10; // XOR -> all zero
+        op = 4'h7; a = 8'hAA; b = 8'hxx;  #10; // NOT
+        op = 4'hE; a = 8'h76; b = 8'h13;  #10; // return A
+        op = 4'hF; a = 8'h76; b = 8'h13;  #10; // return B
 
         $finish;
     end
