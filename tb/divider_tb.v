cd35829559ee775d00658f33942968db552a69b3
Shiwam Dutta <shiwamdutta3277@gmail.com>
2026-05-31 14:48:16 +0530

    test: refine divider 8-bit test cases

--- tb/divider_tb.v
+++ tb/divider_tb.v
@@ -1,26 +1,27 @@
 `timescale 1ns/1ps
 
 module divider_tb;
+    localparam WIDTH = 8;
 
-    reg  signed [7:0] d, m;
-    wire signed [7:0] q, r;
+    reg  signed [WIDTH-1:0] d, m;
+    wire signed [WIDTH-1:0] q, r;
     wire div_by_zero, overflow;
 
-    divider #(8) uut (d, m, q, r, div_by_zero, overflow);
+    divider #(WIDTH) uut (d, m, q, r, div_by_zero, overflow);
 
     initial begin
         $dumpfile("waves/divider.vcd");
         $dumpvars(0, uut);
 
         $monitor("%d = %d * %d + %d", d, m, q, r);
-        d =  25; m =  5; #10;
-        d =  99; m =  7; #10;
-        d = 120; m =  8; #10;
-        d =-100; m = 15; #10; // negative quotient
-        d =-110; m =-13; #10; // negative remainder
-        d =-128; m =- 1; #10; // overflow condition
-        d =  64; m =  3; #10;
-        d = 100; m =  0; #10; // divided by 0
+        d =   25; m =   5;  #10;  // zero remainder
+        d =   99; m =   7;  #10;  // non-zero remainder
+        d =  100; m = -15;  #10;  // negative quotient, positive remainder
+        d = -100; m =  15;  #10;  // negative quotient, negative remainder
+        d = -100; m = -15;  #10;  // positive quotient, negative remainder
+        d =    0; m =  25;  #10;  // zero dividend
+        d = -128; m =  -1;  #10;  // signed overflow
+        d =  100; m =   0;  #10;  // divided by zero
 
         $finish;
 
